ff04bc5003f033483a5c3df3a205a3836a979fc0
Sergey Asryan <asryan.sergey@gmail.com>
2021-12-04 17:42:56 +0400

    Update solution for day_3

--- src/day_3/binary_diagnostic.py
+++ src/day_3/binary_diagnostic.py
@@ -11,12 +11,8 @@ def calc_power_consumption_part_1(power_data: List[int]) -> int:
     line_len = len(power_data[0])
     zero_count = 0
     for idx in range(line_len):
-        zero_count = 0
-        for num in power_data:
-            if num[idx] == "0":
-                zero_count += 1
-        one_count = len(power_data) - zero_count
-        if one_count > zero_count:
+        zero_count = len([x for x in power_data if x[idx] == "0"])
+        if len(power_data) - zero_count > zero_count:
             gamma_rates += "1"
             epsilion_rates += "0"
         else:
@@ -25,50 +21,29 @@ def calc_power_consumption_part_1(power_data: List[int]) -> int:
     return int(gamma_rates, 2) * int(epsilion_rates, 2)
 
 
-def calc_oxygen_rate(power_data: List[int], start_idx: int) -> int:
+def calc_target_rating(power_data: List[int], start_idx: int, target: int) -> int:
     if len(power_data) == 1:
         return int(power_data[0], 2)
-    line_len = len(power_data[0])
-    zero_count = 0
-    ol, co2l = [], []
-    for idx in range(start_idx, line_len):
-        zero_count = 0
-        for pos, num in enumerate(power_data):
-            if num[idx] == "0":
-                zero_count += 1
-                co2l.append(num)
-            else:
-                ol.append(num)
-        one_count = len(power_data) - zero_count
-        if one_count >= zero_count:
-            return calc_oxygen_rate(ol, start_idx + 1)
-        else:
-            return calc_oxygen_rate(co2l, start_idx + 1)
 
-
-def calc_co2_rate(power_data: List[int], start_idx: int) -> int:
-    if len(power_data) == 1:
-        return int(power_data[0], 2)
     line_len = len(power_data[0])
-    zero_count = 0
-    ol, co2l = [], []
+    zero_elems, one_elems = [], []
     for idx in range(start_idx, line_len):
-        zero_count = 0
-        for pos, num in enumerate(power_data):
-            if num[idx] == "0":
-                zero_count += 1
-                co2l.append(num)
-            else:
-                ol.append(num)
-        one_count = len(power_data) - zero_count
-        if one_count >= zero_count:
-            return calc_co2_rate(co2l, start_idx + 1)
-        else:
-            return calc_co2_rate(ol, start_idx + 1)
+        zero_elems = list(filter(lambda x: x[idx] == "0", power_data))
+        one_elems = list(filter(lambda x: x[idx] == "1", power_data))
+        one_count = (len(power_data) - len(zero_elems)) // (len(power_data) // 2)
+        cond = one_count if target else 1 - one_count
+        return (
+            calc_target_rating(one_elems, start_idx + 1, target)
+            if cond
+            else calc_target_rating(zero_elems, start_idx + 1, target)
+        )
 
 
 def calc_power_consumption_part_2(power_data: List[int]) -> int:
-    return calc_oxygen_rate(power_data, 0) * calc_co2_rate(power_data, 0)
+    start_idx = 0
+    oxygen_rating = calc_target_rating(power_data, start_idx, target=0)
+    co2_rating = calc_target_rating(power_data, start_idx, target=1)
+    return oxygen_rating * co2_rating
 
 
 if __name__ == "__main__":
